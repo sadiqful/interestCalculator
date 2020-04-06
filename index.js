4893d7dcef41759b60b95fcb5c59366bc853e0c6
Aliyu Abubakar <sadiqful@gmail.com>
2020-04-06 00:07:19 +0000

    data arranged

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 let data = [
-  {'principal': 2500, 
-    'time': 1.8 
-    },
-
-  {'principal': 1000, 
-  'time': 5 
-  },
-
-  {'principal': 3000, 
-  'time': 1 
-  },
-
-  {'principal': 2000, 
-  'time': 3 
-  },
+  {'principal': 2500, 'time': 1.8 },
+  {'principal': 1000, 'time': 5 },
+  {'principal': 3000, 'time': 1 },
+  {'principal': 2000, 'time': 3 },
 ]
 
 function interestCalculator (arr) {
